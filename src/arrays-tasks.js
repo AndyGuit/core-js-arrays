a1f9e1b2f037623256949a253c97f03426770b7e
AndyGuit <totalslash@msn.com>
2024-02-09 03:25:31 +0200

    feat: implement findLongestIncreasingSubsequence

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -503,8 +503,18 @@ function findCommonElements(arr1, arr2) {
  *    findLongestIncreasingSubsequence([3, 10, 2, 1, 20]) => 2
  *    findLongestIncreasingSubsequence([50, 3, 10, 7, 40, 80]) => 3
  */
-function findLongestIncreasingSubsequence(/* nums */) {
-  throw new Error('Not implemented');
+function findLongestIncreasingSubsequence(nums) {
+  let count = 1;
+
+  return nums.reduce((acc, _, i) => {
+    if (nums[i] > nums[i - 1]) {
+      count += 1;
+    } else {
+      count = 1;
+    }
+
+    return count > acc ? count : acc;
+  }, 0);
 }
 
 /**
