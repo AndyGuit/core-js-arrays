60a0f3efa3541a19ca9aa4adf82870bb545be5c4
AndyGuit <totalslash@msn.com>
2024-02-09 02:07:38 +0200

    feat: add shiftArray implementation

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -540,8 +540,19 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  let left;
+  let right;
+
+  if (n > 0) {
+    left = arr.slice(0, n + 1);
+    right = arr.slice(n + 1);
+  } else {
+    left = arr.slice(0, Math.abs(n));
+    right = arr.slice(Math.abs(n));
+  }
+
+  return [...right, ...left];
 }
 
 /**
